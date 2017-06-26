4494d07c6a9e2d9279aff8604a81f23dbb317db3
eit <eit@MCEIT02.eur.ad.sag>
2017-06-26 17:05:14 +0300

    updated to v1.5

--- TestProj/src/my/Calculator.java
+++ TestProj/src/my/Calculator.java
@@ -3,11 +3,11 @@ package my;
 public class Calculator {
 	public int calculate(int a, int b)
 	{
-		return (a+b)*2;
+		return (a+b)/2;
 	}
 	
 	public static String getVersion()
 	{
-		return "1.4";
+		return "1.5";
 	}
 }
